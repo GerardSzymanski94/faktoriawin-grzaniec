345805b9e5e6f06e6ab68f0fe2bdaf13f6e8e165
GerardSzymanski94 <gerard.szymanski1@gmail.com>
2022-01-07 18:14:56 +0100

    Seeder do admina

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::updateOrCreate([
+            'name'=>'admin',
+            'email'=>'admin@admin.pl',
+            'password'=>Hash::make('123qwe')
+        ]);
+    }
+}
